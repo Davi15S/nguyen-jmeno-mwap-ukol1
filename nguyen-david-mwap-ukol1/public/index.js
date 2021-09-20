eff395ce039ff36ffb0e35547dfa52a58226b0d4
Davi15S <81320898+Davi15S@users.noreply.github.com>
2021-09-20 17:02:55 +0200

    Add files via upload

---
+++ nguyen-david-mwap-ukol1/public/index.js
@@ -0,0 +1,29 @@
+const ar = ['Lodash', 'React', 'Next', 'Strapy', 'Axios', 'Typescript'];
+const rev = ar.reverse();
+
+for (let step = 0; step < 6; step++) {
+  console.log(ar[step]);
+}
+
+for (let step = 0; step < 6; step++) {
+  console.log(rev[step]);
+}
+
+const car1 = {
+  type: "Fiat",
+  model: "500",
+  color: "white"
+};
+const car2 = {
+  type: "Skoda",
+  model: "Fabia",
+  color: "red"
+};
+const car3 = {
+  type: "Audi",
+  model: "R6",
+  color: "black"
+};
+const rn = 0;
+rn = Math.floor(Math.random() * 556);
+console.log('Náhodné číslo je:' + rn);
